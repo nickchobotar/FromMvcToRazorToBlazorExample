b474e1fb194d3657840cbaa928be78549c2f4aea
Nick Chobotar <nick.chobotar@outlook.com>
2020-02-23 22:47:15 -0500

    Add project files.

---
+++ counterRazor/Pages/Counter.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace counterRazor
+{
+    public class CounterModel : PageModel
+    {
+        public int Number { get; set; }
+
+        public void OnPost()
+        {
+            int number = int.Parse(Request.Form["number"]);
+            Number = number + 1;
+        }
+    }
+}
+
+ 
